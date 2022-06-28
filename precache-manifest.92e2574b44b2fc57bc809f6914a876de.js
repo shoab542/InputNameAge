2ed886e1c7152e55ae5396f7131aa1102a73da5b
Shoab <shoab542@gmail.com>
2022-06-29 00:00:46 +0600

    Updates

---
+++ precache-manifest.92e2574b44b2fc57bc809f6914a876de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c6a6d2c47df3f04c17173c74da5629",
+    "url": "/InputNameAge/index.html"
+  },
+  {
+    "revision": "ef2b0a95ca5a8de1fa48",
+    "url": "/InputNameAge/static/css/main.734f694f.chunk.css"
+  },
+  {
+    "revision": "80e500c65e65b137103a",
+    "url": "/InputNameAge/static/js/2.ed9d6333.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/InputNameAge/static/js/2.ed9d6333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2b0a95ca5a8de1fa48",
+    "url": "/InputNameAge/static/js/main.6bd66722.chunk.js"
+  },
+  {
+    "revision": "a2796d560607b73813b4",
+    "url": "/InputNameAge/static/js/runtime-main.78dfeaac.js"
+  }
+]);
